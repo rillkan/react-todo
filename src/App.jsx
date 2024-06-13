3c96f8b9881d1640bbbf8130dd56f303f0074697
Reagan Kan <mjkan96@gmail.com>
2024-06-13 17:58:17 +0800

    react-todo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='header-container'>
+        <div className='header'>
+          <p className='header-title'>
+            My Tasks
+          </p>
+          <div className='header-add-task'>
+            <p className='header-add-task-text'>
+              +
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
